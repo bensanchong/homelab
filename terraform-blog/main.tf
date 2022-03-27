33d8f40761ec4f11256b48e09f1102094181c8c4
Bensan Chong~ <bensanchong@gmail.com>
2022-03-27 08:51:59 -0400

    delete and clean

--- terraform-blog/main.tf
+++ terraform-blog/main.tf
@@ -12,10 +12,10 @@ provider "proxmox" {
   pm_api_url = "https://home-node2.cloud-labhome.net:8006/api2/json"
 
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = "terraform_svc@pam!tc_svc"
+  pm_api_token_id = "tokenuser"
 
   # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = "3a370e53-6522-43b9-ba31-6b6f78ddf824"
+  pm_api_token_secret = "APIKEY"
 
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
